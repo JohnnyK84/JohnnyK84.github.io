d63bc4173207cc6086a6563cca21393bc88e146e
Johnny Kingi <johnny.kingi@rezare.com>
2019-11-05 16:23:18 +1300

    trying with Gallaher stick reader

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 function scan() {
   navigator.bluetooth
-    .requestDevice({ filters: [{ namePrefix: "RS" }] })
+    .requestDevice({ filters: [{ namePrefix: "GGL HR5" }] })
     .then(result => {
       alert(result);
     })
     .catch(err => {
       alert(err);
     });
-  alert("Clicked");
 }
