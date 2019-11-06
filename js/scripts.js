b57ef0113d4b2da5fff77d47821f375fcd59bfc7
Johnny Kingi <johnny.kingi@rezare.com>
2019-11-06 14:03:14 +1300

    name and service snpshot

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,13 @@ function scan() {
       filters: [
         {
           acceptAllDevices: true,
-          name: "SNPSHOT Sampler"
+          name: "SNPSHOT Sampler",
+          services: "66021000-43af-49c1-A7bc-cef71abd0ad9"
         }
       ]
     })
     .then(result => {
-      //alert(result);
+      alert(result);
     })
     .catch(err => {
       alert(err);
