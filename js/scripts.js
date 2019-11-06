60a076b0d50ec14831e111035a2d189b9b2ffd84
Johnny Kingi <johnny.kingi@rezare.com>
2019-11-06 14:05:01 +1300

    name and service snpshot for got []

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function scan() {
         {
           acceptAllDevices: true,
           name: "SNPSHOT Sampler",
-          services: "66021000-43af-49c1-A7bc-cef71abd0ad9"
+          services: ["66021000-43af-49c1-A7bc-cef71abd0ad9"]
         }
       ]
     })
