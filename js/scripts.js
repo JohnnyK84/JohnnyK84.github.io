b8ba681409f045462399eb108cc2baaea32fb162
Johnny Kingi <johnny.kingi@rezare.com>
2019-11-06 13:55:00 +1300

    batter_service only

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,7 @@ function scan() {
       filters: [
         {
           acceptAllDevices: true,
-          services: [
-            "66021000-43af-49c1-a7bc-cef71abd0ad9",
-            "46021000-43af-49c1-a7bc-cef71abd0ad9",
-            "battery_service"
-          ]
+          services: ["battery_service"]
         }
       ]
     })
