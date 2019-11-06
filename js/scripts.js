4fb73dc078404427ef3890b70246fdfdcb9a8d08
Johnny Kingi <johnny.kingi@rezare.com>
2019-11-06 13:46:58 +1300

    change filter

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,11 @@ function scan() {
     .requestDevice({
       filters: [
         {
+          acceptAllDevices: true,
           services: [
             "66021000-43af-49c1-a7bc-cef71abd0ad9",
             "46021000-43af-49c1-a7bc-cef71abd0ad9"
           ]
-        },
-        {
-          acceptAllDevices: true
         }
       ]
     })
